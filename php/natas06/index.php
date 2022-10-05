11425e6477e14364d9be6237197ef3c93f4ec30a
Michael Zhang <mail@mzhang.io>
2022-10-05 14:59:02 -0500

    fix up natas06

--- php/natas06/index.php
+++ php/natas06/index.php
@@ -1,36 +1,32 @@
 <html>
-<head>
-<!-- This stuff in the header has nothing to do with the level -->
-<link rel="stylesheet" type="text/css" href="http://natas.labs.overthewire.org/css/level.css">
-<link rel="stylesheet" href="http://natas.labs.overthewire.org/css/jquery-ui.css" />
-<link rel="stylesheet" href="http://natas.labs.overthewire.org/css/wechall.css" />
-<script src="http://natas.labs.overthewire.org/js/jquery-1.9.1.js"></script>
-<script src="http://natas.labs.overthewire.org/js/jquery-ui.js"></script>
-<script src=http://natas.labs.overthewire.org/js/wechall-data.js></script><script src="http://natas.labs.overthewire.org/js/wechall.js"></script>
-<script>var wechallinfo = { "level": "natas6", "pass": "<censored>" };</script></head>
-<body>
-<h1>natas6</h1>
-<div id="content">
+  <head>
+    <!-- This stuff in the header has nothing to do with the level -->
+    <link rel="stylesheet" type="text/css" href="../natas.css" />
+  </head>
+  <body>
+    <h1>natas6</h1>
+    <div id="content">
 
-<?php
+      <?php
 
-include "includes/secret.inc";
+      include "includes/secret.inc";
 
-    if(array_key_exists("submit", $_POST)) {
+      if(array_key_exists("submit", $_POST)) {
         if($secret == $_POST['secret']) {
-        print "Access granted. The password for natas7 is <censored>";
-    } else {
-        print "Wrong secret";
-    }
-    }
-?>
+          $flag = getenv("NATAS06");
+          print "Access granted. The flag is <code>$flag</code>.";
+        } else {
+          print "Wrong secret";
+        }
+      }
+      ?>
 
-<form method=post>
-Input secret: <input name=secret><br>
-<input type=submit name=submit>
-</form>
+      <form method="post">
+        Input secret: <input name="secret" /><br />
+        <input type="submit" name="submit" />
+      </form>
 
-<div id="viewsource"><a href="index-source.html">View sourcecode</a></div>
-</div>
-</body>
+      <div id="viewsource"><a href="./source.php">View sourcecode</a></div>
+    </div>
+  </body>
 </html>
